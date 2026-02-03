424a9805c9b85d02508f6ead2490460e8e0f2056
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-02-04 02:31:41 +0900

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -286,9 +286,8 @@ function applyWholesome(str) {
     window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
   };
  
-}
- 
- ()
+}());
+
 
 document.addEventListener('mousemove', e => {
     document.querySelectorAll('.mytooltiptext').forEach(el => {
@@ -296,5 +295,3 @@ document.addEventListener('mousemove', e => {
         el.style.setProperty('--mouse-y', e.clientY + 'px');
     });
 });
- 
-);
