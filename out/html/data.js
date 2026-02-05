9b643b2db3f22b1302069757d40daf86396f9daa
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-02-05 11:55:50 +0900

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,9 +1,18 @@
 // Political Party JS
-// FAUD
 
 const tooltipList = [{
     searchString: "FAUD",
     explanationText: "<img src=img/ps_a.png> The Free Workers Association of Germany. <br> (Freie Arbeitervereinigung Deutschlands)"
+},
+                     
+{
+    searchString: "SP",
+    explanationText: "<img src=img/ps_a.png> The Socialist Party of Germany. <br> ()"
+},
+
+{
+    searchString: "SPD",
+    explanationText: "<img src=img/ps_a.png> The Social Democratic Party. <br> ()"
 }];
 
 
@@ -11,200 +20,19 @@ const colourList = [{
         word: "FAUD",
         style: "color: #690E0E; font-weight: bold;",
         img: "img/ps.png" // this line is not mandatory
-    }];
-
-// Socialist Party
-
-const tooltipList = [{
-    searchString: "SP",
-    explanationText: "<img src=img/ps_a.png> The Socialist Party of Germany. <br> (Sozialistische Partei)"
-}];
-
-
-const colourList = [{
+},
+                
+{
         word: "SP",
-        style: "color: #BA0000; font-weight: bold;",
+        style: "color: #D10000; font-weight: bold;",
         img: "img/ps.png" // this line is not mandatory
-    }];
-
-// SPD
-
-const tooltipList = [{
-    searchString: "SPD",
-    explanationText: "<img src=img/ps_a.png> The Social Democratic Party. <br> ( )"
-}];
-
-
-const colourList = [{
+},
+    
+{
         word: "SPD",
         style: "color: #E30063; font-weight: bold;",
         img: "img/ps.png" // this line is not mandatory
-    }];
-
-// Z
-
-const tooltipList = [{
-    searchString: "Z",
-    explanationText: "<img src=img/ps_a.png> The Centre Party. <br> (Deutsche Zentrumspartei)"
 }];
 
-
-const colourList = [{
-        word: "Z",
-        style: "color: #FFFFFF; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// BVP
-
-const tooltipList = [{
-    searchString: "BVP",
-    explanationText: "<img src=img/ps_a.png> The Bavarian People's Party. <br> (Freie Arbeitervereinigung Deutschlands)"
-}];
-
-
-const colourList = [{
-        word: "BVP",
-        style: "color: #8AF9FF; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// NLP
-
-const tooltipList = [{
-    searchString: "NLP",
-    explanationText: "<img src=img/ps_a.png> The National Liberal Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "NLP",
-        style: "color: #53683B; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// FVP
-
-const tooltipList = [{
-    searchString: "FVP",
-    explanationText: "<img src=img/ps_a.png> Progressive People's Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "FVP",
-        style: "color: #FFED4D; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// Guelphs
-
-const tooltipList = [{
-    searchString: "Guelphs",
-    explanationText: "<img src=img/ps_a.png> The German-Hanoverian Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "Guelphs",
-        style: "color: #B5BA54; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-const tooltipList = [{
-    searchString: "GHP",
-    explanationText: "<img src=img/ps_a.png> The German-Hanoverian Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "GHP",
-        style: "color: #B5BA54; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// Polish Party
-
-const tooltipList = [{
-    searchString: "PP",
-    explanationText: "<img src=img/ps_a.png> The Polish Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "PP",
-        style: "color: #FF96D7; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// Alsace Lorraine
-
-const tooltipList = [{
-    searchString: "ELVP",
-    explanationText: "<img src=img/ps_a.png> The Alsace–Lorraine People's Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "ELVP",
-        style: "color: #000E91; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// DKP
-
-const tooltipList = [{
-    searchString: "DKP",
-    explanationText: "<img src=img/ps_a.png> The German Conservative Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "DKP",
-        style: "color: #274875; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// DVP
-
-const tooltipList = [{
-    searchString: "DVP",
-    explanationText: "<img src=img/ps_a.png> The German Volkisch party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "DVP",
-        style: "color: #161654; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// DVLP
-
-const tooltipList = [{
-    searchString: "DVLP",
-    explanationText: "<img src=img/ps_a.png> The German Fatherland Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "DVLP",
-        style: "color: #426366; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
-
-// DSP
-
-const tooltipList = [{
-    searchString: "DSP",
-    explanationText: "<img src=img/ps_a.png> The German Socialist Party. <br> ()"
-}];
-
-
-const colourList = [{
-        word: "DSP",
-        style: "color: #2E4D1F; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-    }];
 
 
