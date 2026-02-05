dcb6d9bc2b33de53a8b93e09dd52c2a041a4469f
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-02-05 13:21:40 +0900

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -2,140 +2,126 @@
 
 const tooltipList = [{
     searchString: "FAUD",
-    explanationText: "<img src=img/ps_a.png> The Free Workers Association of Germany. <br> (Freie ArbeitervereinigUng Deutschlands)"
+    explanationText: "The Free Workers Association of Germany. <br> (Freie ArbeitervereinigUng Deutschlands)"
 },
                     {
     searchString: "SPD",
-    explanationText: "<img src=img/ps_a.png> The Social Democratic Party of Germany. <br> (Sozialdemokratische Partei Deutschlands)"
+    explanationText: "The Social Democratic Party of Germany. <br> (Sozialdemokratische Partei Deutschlands)"
 },
                     {
     searchString: "SP",
-    explanationText: "<img src=img/ps_a.png> The Free Workers Association of Germany. <br> (Sozialistische Partei)"
+    explanationText: "The Free Workers Association of Germany. <br> (Sozialistische Partei)"
 },
                      {
     searchString: "Z",
-    explanationText: "<img src=img/ps_a.png> The Centre Party. <br> (Deutsche Zentrumspartei)"
+    explanationText: "The Centre Party. <br> (Deutsche Zentrumspartei)"
 },
                     {
     searchString: "BVP",
-    explanationText: "<img src=img/ps_a.png> The Bavarian People's Party. <br> (Freie Arbeitervereinigung Deutschlands)"
+    explanationText: "The Bavarian People's Party. <br> (Freie Arbeitervereinigung Deutschlands)"
 },
                     {
     searchString: "NLP",
-    explanationText: "<img src=img/ps_a.png> The National Liberal Party. <br> (Nationalliberale Partei)"
+    explanationText: "The National Liberal Party. <br> (Nationalliberale Partei)"
 },
                     {
     searchString: "FVP",
-    explanationText: "<img src=img/ps_a.png> The Progressive People's Party <br> (Fortschrittliche Volkspartei)"
+    explanationText: "The Progressive People's Party <br> (Fortschrittliche Volkspartei)"
 },
                     {
     searchString: "Guelph",
-    explanationText: "<img src=img/ps_a.png> The Guelphs Party, otherwise known as the German-Hanoverian Party. <br> (Deutsch-Hannoversche Partei/Welfenpartei)"
+    explanationText: "The Guelphs Party, otherwise known as the German-Hanoverian Party. <br> (Deutsch-Hannoversche Partei/Welfenpartei)"
 },
                      {
     searchString: "GHP",
-    explanationText: "<img src=img/ps_a.png> The Guelphs Party, otherwise known as the German-Hanoverian Party. <br> (Deutsch-Hannoversche Partei/Welfenpartei)"
+    explanationText: "The Guelphs Party, otherwise known as the German-Hanoverian Party. <br> (Deutsch-Hannoversche Partei/Welfenpartei)"
 },
                     {
     searchString: "PP",
-    explanationText: "<img src=img/ps_a.png> The Polish Party. <br> (Polnische Partei/Partia Polska)"
+    explanationText: "The Polish Party. <br> (Polnische Partei/Partia Polska)"
 },
                     {
     searchString: "ELVP",
-    explanationText: "<img src=img/ps_a.png> The Alsace-Lorraine People's Party. <br> (Elsass-Lothringen VolksPartei)"
+    explanationText: "The Alsace-Lorraine People's Party. <br> (Elsass-Lothringen VolksPartei)"
 },
                     {
     searchString: "DKP",
-    explanationText: "<img src=img/ps_a.png> The German Conservative Party. <br> (Deutsche Konservative Partei)"
+    explanationText: "The German Conservative Party. <br> (Deutsche Konservative Partei)"
 },
                     {
     searchString: "DVP",
-    explanationText: "<img src=img/ps_a.png> The German Völkisch Party. <br> (Deutschvölkische Partei)"
+    explanationText: "The German Völkisch Party. <br> (Deutschvölkische Partei)"
 },
                      {
     searchString: "DVLP",
-    explanationText: "<img src=img/ps_a.png> The German Fatherland Party. <br> (Deutsche VaterLandsPartei)"
+    explanationText: "The German Fatherland Party. <br> (Deutsche VaterLandsPartei)"
 },
                     {
     searchString: "DSP",
-    explanationText: "<img src=img/ps_a.png> The German Socialist Party. <br> (Deutsche Sozialistische Partei)"
+    explanationText: "The German Socialist Party. <br> (Deutsche Sozialistische Partei)"
 }];
 
 
 const colourList = [{
         word: "FAUD",
         style: "color: #690E0E; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "SP",
         style: "color: #D10000; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "SPD",
         style: "color: #E30063; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "Z",
         style: "color: #FFFFFF; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "BVP",
         style: "color: #8AF9FF; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "NLP",
         style: "color: #53683B; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "FVP",
         style: "color: #FFED4D; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "Guelph",
         style: "color: #B5BA54; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "GHP",
         style: "color: #B5BA54; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "PP",
         style: "color: #FF96D7; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "ELVP",
         style: "color: #000E91; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "DKP",
         style: "color: #274875; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "DVLP",
         style: "color: #426366; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "DVP",
         style: "color: #161654; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
 },
                     {
         word: "DSP",
         style: "color: #2E4D1F; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
+       // img: "img/ps.png"
 },
 ];;
 
