1354b94d0aa85f38ab79c09ca444919cdd8ed3b7
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-02-05 11:59:50 +0900

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -3,16 +3,6 @@
 const tooltipList = [{
     searchString: "FAUD",
     explanationText: "<img src=img/ps_a.png> The Free Workers Association of Germany. <br> (Freie Arbeitervereinigung Deutschlands)"
-},
-                     
-{
-    searchString: "SP",
-    explanationText: "<img src=img/ps_a.png> The Socialist Party of Germany. <br> ()"
-},
-
-{
-    searchString: "SPD",
-    explanationText: "<img src=img/ps_a.png> The Social Democratic Party. <br> ()"
 }];
 
 
@@ -20,19 +10,7 @@ const colourList = [{
         word: "FAUD",
         style: "color: #690E0E; font-weight: bold;",
         img: "img/ps.png" // this line is not mandatory
-},
-                
-{
-        word: "SP",
-        style: "color: #D10000; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-},
-    
-{
-        word: "SPD",
-        style: "color: #E30063; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
-}];
+}];;
 
 
 
